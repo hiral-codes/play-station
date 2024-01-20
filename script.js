19742c7c01d91974bb5c7960078ef75b3a65cf4d
hiral-codes <hiralpatel160302@gmail.com>
2024-01-20 22:55:44 +0530

    Added Files

---
+++ script.js
@@ -0,0 +1,17 @@
+const header = document.querySelector(".navbar");
+const main = document.querySelector("main");
+window.addEventListener("scroll", () => {
+    header.style.position = window.scrollY > 36 ? "fixed" : "relative";
+    header.style.top = window.scrollY > 36 ? "0" : "";
+});
+
+function toggleIcon(clickedElement) {
+    document.querySelectorAll('.menu-item').forEach(function (element) {
+        if (element !== clickedElement) {
+            const icon = element.querySelector('.icon');
+            icon.classList.remove('rotate-icon');
+        }
+    });
+    const clickedIcon = clickedElement.querySelector('.icon');
+    clickedIcon.classList.toggle('rotate-icon');
+}
