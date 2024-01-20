75b3460d7b58ab7f799517622358adbe628ac527
hiral-codes <hiralpatel160302@gmail.com>
2024-01-20 23:05:53 +0530

    Added Padding to main div on scroll

--- script.js
+++ script.js
@@ -1,8 +1,10 @@
 const header = document.querySelector(".navbar");
 const main = document.querySelector("main");
+
 window.addEventListener("scroll", () => {
     header.style.position = window.scrollY > 36 ? "fixed" : "relative";
     header.style.top = window.scrollY > 36 ? "0" : "";
+    main.classList.toggle("margin", window.scrollY > 36);
 });
 
 function toggleIcon(clickedElement) {
